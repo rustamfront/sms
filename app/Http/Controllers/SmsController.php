551b43f6e6a9004ea9011a4f6f2ff2fc43b8f7cb
rustam <rustamfrontend@gmail.com>
2024-12-18 13:18:03 +0300

    first commit

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -9,9 +9,9 @@ class SmsController extends Controller
 {
     public function send(Request $request, string $action)
     {
-        $url = config('services.api.url');
+        $url = 'https://postback-sms.com/api/';
         $data = $request->all();
-        $data['token'] = config('services.api.token');
+        $data['token'] = '5994c91001f57eea808aff11738d752a';
         $data['action'] = $action;
 
         try {
